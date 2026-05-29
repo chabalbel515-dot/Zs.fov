eb6caa31e583426fdbea90a681ec69621af283d1
Z.sXis <chabalbel515@gmail.com>
2026-05-29 17:25:03 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const mira = document.getElementById('mira');
-
-mira.addEventListener('touchstart', function(e) {
-    let touch = e.touches[0];
-    mira.startX = touch.clientX;
-    mira.startY = touch.clientY;
-});
-
-mira.addEventListener('touchmove',
